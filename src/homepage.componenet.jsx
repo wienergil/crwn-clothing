6e88cd2185da62c740dc10e5ce1b687b89635de5
wienergil <wienergil@gmail.com>
2021-02-06 00:34:27 +0200

    Test Commit

--- src/homepage.componenet.jsx
+++ src/homepage.componenet.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import './homepage.styles.scss'
 
-const HomePage = () => (
+const HomePage = (props) => (
     <div className='homepage'>
         <div className='directory-menu'>
             <div className='menu-item'>
