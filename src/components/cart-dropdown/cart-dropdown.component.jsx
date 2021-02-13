360bec61a43842bb46d9a4af6e6f832bbaf1f755
wienergil <wienergil@gmail.com>
2021-02-13 13:51:42 +0200

    add cart-item componenet

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import CustomButton from '../custom-button/custom-button.component'
 import { connect } from 'react-redux'
 import CollectionItem from '../collection-item/collection-item'
-
+import CartItem from "../cart-item/cart-item.componenet";
 
 
 import './cart-dropdown.styles.scss'
@@ -11,7 +11,7 @@ const CartDropdown = ({ cartItems }) => (
     <div className='cart-dropdown'>
         <div className='cart-items'>
             {cartItems ? cartItems.map((item) => (
-                <CollectionItem key={item.id} item={item} />
+                <CartItem key={item.id} item={item} />
             )) : null}
         </div>
         <CustomButton>GO TO CHECKOUT</CustomButton>
